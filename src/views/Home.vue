cc9eb75f093a0a6a02dce3373f3bc07f97516943
Robert <robertneagu194@gmail.com>
2020-02-03 14:43:21 +0000

    Add grid to the main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,20 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <aside class="profile"></aside>
+    <section class="content"></section>
+  </div>
 </template>
+
+<style lang="scss">
+#app .home {
+  display: grid;
+  grid-template:
+            "profile content" 100% / 300px 1fr;
+
+  .profile {
+    display: flex;
+    flex-direction: column;
+    background: linear-gradient(to bottom, darken(teal, 15%) 0%, darken(teal, 14%) 100%);
+  }
+}
+</style>
