904b64c1a37cc1fbabebded971b38c528b9d0bde
Robert <robertneagu194@gmail.com>
2020-02-03 13:48:48 +0000

    Add background color and make content fit the height

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,20 @@
 <style lang="scss">
 @import '@/scss/_normalize.scss';
 
-html, body, #app { min-height: 100vh; }
+html, body, #app {
+  min-height: 100vh;
+  background-color: black;
+}
 #app {
+  display: flex;
+  flex-direction: column;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  text-shadow: 1px 1px 1px black;
+
+  > * { flex: 1; }
 }
 </style>
